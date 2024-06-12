103a4ab64a817b40efb5b77339b9aef6a4454f26
rohitrodge <webrohit27@gmail.com>
2024-06-12 19:37:56 +0530

    node Basic

---
+++ Node/index.js
@@ -0,0 +1,14 @@
+// let user = {
+//     name: Kuldeeep sir,
+//     age : 27,
+//     hobby : ["cricket", "swimming", "Football"]
+// }
+
+// console.log(user);
+
+let arr = [10,20,30,40]
+let a = arr.filter((a)=>{
+    return a>30
+})
+console.log(a)
+
